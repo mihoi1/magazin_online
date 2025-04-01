fa1f92a969f7f22db29c8bb1289ac8930eb81fc7
mihoi1 <mihoi06042005@gmail.com>
2025-04-01 20:51:36 +0300

    Am stilizat paginile

--- magazin_online/magazin_online/settings.py
+++ magazin_online/magazin_online/settings.py
@@ -124,6 +124,9 @@ STATIC_URL = 'static/'
 LOGIN_URL = 'login/'
 LOGIN_REDIRECT_URL = '/'
 
+from django.urls import reverse_lazy
+LOGIN_URL = reverse_lazy('login')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
